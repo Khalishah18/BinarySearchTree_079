0c659e1c87b33dff8f20c9ca1266ed116c3cc4d0
LISA <ASUS@LAPTOP-67G15PD7>
2022-12-26 17:55:39 +0700

    add case'4'

--- BinarySearchTree/Program.cs
+++ BinarySearchTree/Program.cs
@@ -146,7 +146,11 @@ namespace BinarySearchTree
                             x.preorder(x.ROOT);
                         }
                         break;
-
+                    case '4':
+                        {
+                            x.postorder(x.ROOT);
+                        }
+                        break;
                 }
             }
         }
