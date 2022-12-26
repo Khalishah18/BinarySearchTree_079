a069baca840de4a5daa0f466cea111db3ca74366
LISA <ASUS@LAPTOP-67G15PD7>
2022-12-26 17:22:26 +0700

    add class node and public node root

--- BinarySearchTree/Program.cs
+++ BinarySearchTree/Program.cs
@@ -6,7 +6,32 @@ using System.Threading.Tasks;
 
 namespace BinarySearchTree
 {
-    internal class Program
+    /* A Node class consi*/
+    class node
+    {
+        public string info;
+        public node lchild;
+        public node rchild;
+
+        //constructor for the node class
+
+        public node(string i, node l, node r)
+        {
+            info = i;
+            lchild = l;
+            rchild = r;
+        }
+    }
+    class BinaryTree
+    {
+        public node ROOT;
+
+        public BinarySearchTree()
+        {
+            ROOT = null; //initializing root to null
+        }
+    }
+    class Program
     {
         static void Main(string[] args)
         {
