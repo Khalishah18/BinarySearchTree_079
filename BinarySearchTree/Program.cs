f8d0c6bbec35a741ae505951f8754a45d237ff1d
LISA <ASUS@LAPTOP-67G15PD7>
2022-12-26 17:42:42 +0700

    add public void preorder

--- BinarySearchTree/Program.cs
+++ BinarySearchTree/Program.cs
@@ -85,6 +85,20 @@ namespace BinarySearchTree
                 inorder(ptr.rchild);
             }
         }
+        public void preorder(node ptr)
+        {
+            if(ROOT == null)
+            {
+                Console.WriteLine("tree is empty");
+                return;
+            }
+            if (ptr != null)
+            {
+                Console.WriteLine(ptr.info + "");
+                preorder(ptr.lchild);
+                preorder(ptr.rchild);
+            }
+        }
     }
     class Program
     {
